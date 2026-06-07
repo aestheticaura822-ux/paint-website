f8d907039bc8fd7bcc3e0e55e0e945d9afe92d08
aestheticaura822-ux <aestheticaura822-ux@users.noreply.github.com>
2026-06-07 15:31:04 +0500

    Fixed API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'paint-backend-indol.vercel.app';
+const API_URL = 'https://paint-backend-indol.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
